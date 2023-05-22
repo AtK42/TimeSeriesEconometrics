877c45cfc7b2a65e564d44c82427a4250483c2f3
AtK42 <aaron_7@live.de>
2023-05-22 13:25:25 +0200

    add parameters from paper

--- 3rd assignment/pre_assignment3.m
+++ 3rd assignment/pre_assignment3.m
@@ -9,7 +9,7 @@
 %   are simulating, you can simulate T=5000 observations -- or even more, 
 %   all for free.
 
-% The next part is to estimate a student-t-GARCH model based on your 
+% The next part is to estimate a student-t-GARCH model based on your
 %   simulated data. Notice that the true DGP does NOT match the model you 
 %   are estimating. That is okay -- that is reality. The student-t garch 
 %   estimation is very easy to set up -- you just build on the codes I have
@@ -36,14 +36,33 @@ n_paths = 1e3;
 rng(8, 'multFibonacci')
 % set the model parameters
 % % mean
-mu = [.05, .075, .04]';
+% self chosen:
+%mu = [.05, .075, .04]';
+% from paper:
+mu = [.164, -.153, -.865]';
+
 % % weights
-wghts = [15, 20, 4]';
+% self chosen:
+%wghts = [15, 20, 4]';
+%from paper:
+wghts =  [.541, .433, .026]';
+
 % % GARCH coefficients
-gamma0 = [.01, .025, .03]';
-gamma = [0.2, 0.85, 0.45]';
+% self chosen:
+%gamma0 = [.01, .025, .03]';
+% from paper:
+gamma0 = [0, .012, .0332]';
+
+% self chosen:
+%gamma = [0.2, 0.85, 0.45]';
+% from paper:
+gamma = [.022, .197, 1.303]';
+
+% self chosen:
 %Psi = diag([.1,.3,.2]);
-Psi = toeplitz([.1 .3 .2]);
+%Psi = toeplitz([.1 .3 .2]);
+% from paper:
+Psi = diag([.956, .835, .567])';
 
 [sim_values, sim_sigma2] = MN_GARCH11_sim(len_path, mu, wghts, gamma0, gamma, Psi);
 
@@ -62,4 +81,28 @@ title('volatility process')
 
 %% 2) estimation student-t GARCH model
 est_model = estimate(garch('ARCHLags', 1, 'GARCHLags', 1, 'Distribution', 't'), sim_values);
+%%
+% initial parameter values
+theta0 = [0.01, 0.1, 0.9, 8]; % [omega, alpha, beta, df]
+
+% Define the log-likelihood function
+logLikelihood = @(theta) -sum(log(tpdf(sim_values, theta(4)) .* sqrt(theta(2)/(theta(4)-2)) .* exp(-0.5*(sim_values.^2)./(theta(4)-2))));
+
+% Optimize the log-likelihood function
+options = optimset('MaxIter', 1000, 'MaxFunEvals', 1000);
+theta_hat = fminsearch(logLikelihood, theta0, options);
+
+% Extract the estimated parameters
+omega_hat = theta_hat(1);
+alpha_hat = theta_hat(2);
+beta_hat = theta_hat(3);
+df_hat = theta_hat(4);
+
+% Display the estimated parameters
+fprintf('Estimated GARCH(1,1) coefficients:\n');
+fprintf('omega_hat = %.4f\n', omega_hat);
+fprintf('alpha_hat = %.4f\n', alpha_hat);
+fprintf('beta_hat = %.4f\n', beta_hat);
+fprintf('df_hat = %.4f\n', df_hat);
+
 %% 3) VaR plots
